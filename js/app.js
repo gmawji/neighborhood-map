d3b676bc72cbe0ffee623d90ac9756f26c68be14
gmawji <git@gmawji.com>
2017-07-06 01:42:06 -0400

    Intial Locations around Canary Wharf This is just a start list, some lat,lng could be wrong, not notated properly either.

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// Locations we show by default, in this case my favorite locations!
+var myLocations = [
+    {
+        title: 'Discovery Docks Apartments',
+        lat:51.501409,
+        lng: -0.018823,
+        type:'Living Quarters'
+    },
+    {
+        title: 'Nandos Restaurant',
+        lat:51.5023146,
+        lng:-0.0187593,
+        type:'Restaurant'
+    },
+    {
+        title: 'The Slug and Lettuce',
+        lat:51.5044416,
+        lng:-0.0200729,
+        type:'Restaurant'
+    },
+    {
+        title: 'Canary Wharf Tube Station',
+        lat:51.5034898,
+        lng:-0.0185944,
+        type: 'Transit'
+    },
+    {
+        title: 'One Canada Square',
+        lat:51.5049494,
+        lng:-0.0194997,
+        type: 'Shopping'
+    }
+];
